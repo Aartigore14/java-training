82f6c8c6445bd021a4dbb4649717f9b73822e807
Aarti Gore <aartigore2005@gmail.com>
2026-07-28 20:40:32 +0530

    Java code for Student class using encapsulation

--- .idea/Student.java
+++ .idea/Student.java
@@ -3,5 +3,44 @@ public class Student{
     private String name;
     private double marks;
 
-    public Student
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMarks(double marks) {
+        if (marks >=0 && marks <= 100) {
+            this.marks = marks;
+        }
+        else{
+            System.out.println("Invalid Marks..enter marks between 0 to 100");
+        }
+    }
+
+    static void main() {
+        Student s = new Student();
+        s.setId(1);
+        s.setName("Aarti");
+        s.setMarks(89);
+        System.out.println("Student Details");
+        System.out.println("ID: "+s.getId());
+        System.out.println("Name: "+s.getName());
+        System.out.println("Marks: "+s.getMarks());
+    }
+
 }
